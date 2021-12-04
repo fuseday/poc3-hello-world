57e967a7741c6d81a393d190ff73ee2b872540c8
ijpatricio <ijpatricio@gmail.com>
2021-12-04 22:54:57 +0000

    wip

--- app/Poc3HelloWorldServiceProvider.php
+++ app/Poc3HelloWorldServiceProvider.php
@@ -23,9 +23,11 @@ class Poc3HelloWorldServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app->make('torque')
+        rescue(fn() => $this->app
+            ->make('torque')
             ->registerComponent('fuseday.poc3-hello-world', [
                 '<script src="/_torque/lego/fuseday/poc3-hello-world/my-lib.umd.js"></script>',
-            ]);
+            ])
+        );
     }
 }
